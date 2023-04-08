23b15d08ec9e46514bc2857812492136d0f44fa7
Karteek Menda <karteek.menda19@st.niituniversity.in>
2023-04-08 14:27:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
-st.title('🎈 Presentation App')
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQCqeDL5PnYToA6hPJ5YlcT1MiLyqhSqk4S0xqnMmDZ44KYhJ_nYGOZl1gRcz1bhncgN1OilHgi7AjJ/embed?start=false&loop=false&delayms=5000", height=432)
+st.title('Longformers')
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vR2YaFeGq0RdPu9QY194Dfz66AxXP9HVNnxh9BCt4CkbzktUvSKo4ainfEHhnu03wZFjf8p-A0vE1yT/embed?start=false&loop=false&delayms=3000", height=432)
